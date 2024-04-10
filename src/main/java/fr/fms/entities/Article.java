f0e3b5d769f52706f2ec9ac49226e4759352c93e
Gilles <quodbach.gilles@gmail.com>
2024-04-10 10:52:35 +0200

    masterclass ok

--- src/main/java/fr/fms/entities/Article.java
+++ src/main/java/fr/fms/entities/Article.java
@@ -1,5 +1,6 @@
 package fr.fms.entities;
 
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,20 +11,28 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Size;
 
 
 @Entity
 @Data
-@NoArgsConstructor @AllArgsConstructor @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Article implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min = 1, max = 50)
     private String description;
+    @DecimalMin("50")
     private double price;
 
-    public Article(String description, double price){
+
+    public Article(String description, double price) {
         this.description = description;
         this.price = price;
     }
--- src/main/java/fr/fms/web/ArticleController.java
+++ src/main/java/fr/fms/web/ArticleController.java
@@ -7,10 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -20,26 +23,41 @@ public class ArticleController {
 
     // @RequestMapping(value="/index", method=RequestMethod.GET)
     @GetMapping("/index")
-    public String index(Model model, @RequestParam(name="page", defaultValue = "0")int page,
-                        @RequestParam(name="keyword", defaultValue = "")String kw){ //le model est fourni par spring, je peux l'utiliser comme ci
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "keyword", defaultValue = "") String kw) { //le model est fourni par spring, je peux l'utiliser comme ci
         //List<Article> articles = articleRepository.findAll(); //Récup tous les articles
         //Page<Article> articles = articleRepository.findAll(PageRequest.of(page,5));
-        Page<Article> articles = articleRepository.findByDescriptionContains(kw, PageRequest.of(page,5));
+        Page<Article> articles = articleRepository.findByDescriptionContains(kw, PageRequest.of(page, 5));
         model.addAttribute("listArticle", articles.getContent());
 
         model.addAttribute("pages", new int[articles.getTotalPages()]);
 
         model.addAttribute("currentPage", page);
 
-        model.addAttribute("keyword",kw);
+        model.addAttribute("keyword", kw);
 
         // return articles.html
         return "articles"; //cette méthode retourne au dispatcherServerlet une vue
     }
 
     @GetMapping("/delete")
-    public String delete(Long id, int page, String keyword){
+    public String delete(Long id, int page, String keyword) {
         articleRepository.deleteById(id);
-        return "redirect:/index?page="+page+"&keyword="+keyword;
+        return "redirect:/index?page=" + page + "&keyword=" + keyword;
+    }
+
+    @GetMapping("/article")
+    public String article(Model model) {
+        // on inject un article par defaut dans le formulaire de saisi
+        model.addAttribute("article", new Article());
+        return "article";
+    }
+
+    @PostMapping("/save")
+    public String save(Model model, @Valid Article article, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) return "article";
+        // s'il n'y a pas de saisie d'un champ selon certains critères, pas d'insertion en base
+        articleRepository.save(article);
+        return "redirect:/index";
     }
 }
