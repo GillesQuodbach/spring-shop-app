54b363f8a0fe8ef733879a9d06c2901cf2c8ce96
Gilles <quodbach.gilles@gmail.com>
2024-04-10 16:03:24 +0200

    add update methode

--- src/main/java/fr/fms/web/ArticleController.java
+++ src/main/java/fr/fms/web/ArticleController.java
@@ -8,18 +8,27 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ArticleController {
     @Autowired
     ArticleRepository articleRepository;
+
+    public static void setTimeout(Runnable runnable, int delay) {
+        new Thread(() -> {
+            try {
+                Thread.sleep(delay);
+                runnable.run();
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        }).start();
+    }
 
     // @RequestMapping(value="/index", method=RequestMethod.GET)
     @GetMapping("/index")
@@ -40,12 +49,14 @@ public class ArticleController {
         return "articles"; //cette méthode retourne au dispatcherServerlet une vue
     }
 
+    // delete button methode
     @GetMapping("/delete")
     public String delete(Long id, int page, String keyword) {
         articleRepository.deleteById(id);
         return "redirect:/index?page=" + page + "&keyword=" + keyword;
     }
 
+    // add new article
     @GetMapping("/article")
     public String article(Model model) {
         // on inject un article par defaut dans le formulaire de saisi
@@ -53,18 +64,34 @@ public class ArticleController {
         return "article";
     }
 
+    // update button methode
     @GetMapping("/update")
-    public String update(Model model) {
+    public String update(Model model, Long id) {
         // on inject un article par defaut dans le formulaire de saisi
-        Article article = new Article();
+        Optional<Article> articleToUpdate = articleRepository.findById(id);
+        Article article = articleToUpdate.orElse(null);
         model.addAttribute("article", article);
         return "update";
     }
 
+    //New article save methode
     @PostMapping("/save")
-    public String save(Model model, @Valid Article article, BindingResult bindingResult) {
+    public String save(@Valid Article article, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) return "article";
         // s'il n'y a pas de saisie d'un champ selon certains critères, pas d'insertion en base
+        articleRepository.save(article);
+        return "redirect:/index";
+    }
+
+    //article update methode
+    @PostMapping("/toUpdate")
+    public String toUpdate(Long id, @Valid Article updatedArticle, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) return "article";
+        Optional<Article> articleToUpdate = articleRepository.findById(id);
+        Article article = articleToUpdate.get();
+        article.setId(updatedArticle.getId());
+        article.setDescription(updatedArticle.getDescription());
+        article.setPrice(updatedArticle.getPrice());
         articleRepository.save(article);
         return "redirect:/index";
     }
